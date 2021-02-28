7d9d62abfaef30d556bd2ba8cbd1b2c61fb8d268
Ido Nov <idonov8@gmail.com>
2021-02-28 14:00:01 +0200

    improve security by making tokens expire and add server-side logout

--- server.js
+++ server.js
@@ -17,11 +17,7 @@ mongoose.connect(`mongodb+srv://${process.env.MONGO_USERNAME}:${process.env.MONG
 app.use(express.json())
 app.use(cors())
 
-app.get('/', (req, res) => {
-    res.status(200).json({
-        message: 'Test'
-    })
-})
+let refreshTokens = [] // TODO: Move to mongoDB
 
 app.post('/users', async (req,res) => {
     try {
@@ -37,7 +33,11 @@ app.post('/users', async (req,res) => {
             console.log("Created user: ", username)
             res.status(201).send()
         })
-    } catch {
+        .catch(()=> {
+            res.status(409).send('Username taken')
+        })
+    } catch(err) {
+        console.log(err.message)
         res.status(500).send()
     }
 })
@@ -47,23 +47,41 @@ app.post('/login', (req, res) => {
         if (user===null) return res.status(400).send('Cannot find user')
         try {
             if(await bcrypt.compare(req.body.password, user.password)){
-                const accessToken = jwt.sign(user.username, process.env.ACCESS_TOKEN_SECRET)
-                res.json({user, accessToken})
+                const accessToken = generateAccessToken(user)
+                const refreshToken = jwt.sign({user}, process.env.REFRESH_TOKEN_SECRET)
+                refreshTokens.push(refreshToken) // TODO: Move to MongoDB
+                res.json({user, accessToken, refreshToken})
             } else {
                 res.json({error: 'Wrong Password'})
             }
-        } catch {
+        } catch(err) {
+            console.log(err.message)
             res.status(500).send()
         }
     })
     .catch(() => {
         return res.status(500)
     })
-    
 })
 
+app.post('/token', (req, res) => {
+    console.log("Got here")
+    const refreshToken = req.body.refreshToken
+    if (refreshToken == null) return res.sendStatus(401)
+    if (!refreshTokens.includes(refreshToken)) return res.sendStatus(403) // TODO: Check from MongoDB
+    jwt.verify(refreshToken, process.env.REFRESH_TOKEN_SECRET, (err, user) => {
+        if (err) return res.sendStatus(403)
+        const accessToken = generateAccessToken(user)
+        res.json({ user, accessToken })
+    })
+})
+
+app.delete('/logout', (req, res) => {
+    // TODO: delete from MongoDB
+    refreshTokens = refreshTokens.filter(token => token !== req.body.refreshToken)
+    res.sendStatus(204)
+})
 app.get('/auto_login', authenticateToken, (req, res) => {
-    console.log(req.user)
     res.json( { user: req.user, accessToken: req.token})
 })
 
@@ -71,12 +89,16 @@ function authenticateToken(req, res, next){
     const authHeader = req.header('Authorization')
     const token = authHeader && authHeader.split(' ')[1]
     if (token === null) return res.sendStatus(401)
-    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, async (err, username)=> {
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, async (err, {user})=> {
         if (err) return res.sendStatus(403)
-        req.user = await User.findOne({username: username}).exec()
+        req.user = user
         req.token = token
         next()
     })
+}
+
+function generateAccessToken(user) {
+    return jwt.sign({user}, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '30m' })
 }
 
 app.listen(port)
